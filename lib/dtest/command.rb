0bd2eda421b8d2548c276a2eccf13e9890643280
suma <suma@users.sourceforge.jp>
2012-03-07 18:11:44 +0900

    Fixed to show help message, version and banner

--- lib/dtest/command.rb
+++ lib/dtest/command.rb
@@ -1,41 +1,25 @@
-
 require 'optparse'
 require 'dtest/runner'
 require 'dtest/dsl'
 
-def usage
-  puts 'dtest [options] [files...]'
-  puts 'dtest [options] [files...] -- [arguments for testcode]'
-  puts ''
-  puts '  --xml output_path   Specify path to write junit style xml.'
-  puts '  --color             Enables to print color on the terminal.'
+# Command line parser
+argv = ARGV
+first = ARGV.index('--')
+unless first.nil?
+  argv = ARGV[0..first-1]
+  ARGV.slice!(0..first)
 end
+optparse, option = DTest::Runner.parse!(argv)
 
-begin
-  # Command line parser
-  argv = ARGV
-  first = ARGV.index('--')
-  unless first.nil?
-    argv = ARGV[0..first-1]
-    ARGV.slice!(0..first)
+if argv.empty? && !option[:print]
+  puts optparse
+elsif !option[:print]
+  # execute test
+  res = DTest::Runner.run(argv)
+  DTest::Runner.report(res)
+
+  # output xml
+  if option[:xml_path]
+    res.outputxml(option[:xml_path])
   end
-  option = DTest::Runner.parse!(argv)
-
-  if argv.empty?
-    usage
-  else
-    # execute test
-    res = DTest::Runner.run(argv)
-    DTest::Runner.report(res)
-
-    # output xml
-    if option[:xml_path]
-      res.outputxml(option[:xml_path])
-    end
-  end
-rescue OptionParser::InvalidOption => e
-  puts "#{e.message}\n\n"
-  usage
 end
-
-
--- lib/dtest/runner.rb
+++ lib/dtest/runner.rb
@@ -1,6 +1,5 @@
-
-
 require 'optparse'
+require 'dtest/version'
 require 'dtest/core'
 require 'dtest/progress'
 
@@ -11,15 +10,44 @@ module DTest
       res = {}
       xml_path = nil
       opt = OptionParser.new
-      opt.on('--color') do |b|
+      opt.banner = <<EOS
+=====================================
+ ______ _______
+(______|_______)          _
+ _     _   _ _____  ___ _| |_
+| |   | | | | ___ |/___|_   _)
+| |__/ /  | | ____|___ | | |_
+|_____/   |_|_____|___/   \__)  #{DTest::VERSION}
+=====================================
+
+dtest [options] [files...]
+dtest [options] [files...] -- [arguments for testcode]
+EOS
+
+      opt.version = DTest::VERSION
+      opt.on('--color', 'Enables to print color on the terminal') do |b|
         DTest::Report.color_enabled = b
       end
-      opt.on('--xml PATH') do |path|
+      opt.on('--xml PATH', 'Specify path to write junit style xml') do |path|
         res[:xml_path] = path
       end
-      opt.parse!(argv)
+      opt.on_tail('-h', '--help','Show this message') do
+        puts opt
+        res[:print] = true
+      end
+      opt.on_tail('-v', '--version', 'Show version') do
+        puts opt.ver
+        res[:print] = true
+      end
 
-      res
+      begin
+        opt.parse!(argv)
+      rescue OptionParser::InvalidOption => e
+        puts "#{e.message}\n\n"
+        puts optparse
+      end
+
+      [opt, res]
     end
 
     def self.run(files = [])
